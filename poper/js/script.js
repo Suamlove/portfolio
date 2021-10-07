c9d9ae3d5f5eb508266386c389638630cffea7ef
Suamlove <sssonda123@gmail.com>
2021-10-07 14:11:36 +0900

    수정하기

---
+++ poper/js/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+// slide//
+$('.music-slide').slick({
+    autoplay:true,
+    arrows:false,
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  });
+
+
+  $('.gbs_slide').slick({
+    arrows: false,
+    dots:false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+
+
+
+
+});
